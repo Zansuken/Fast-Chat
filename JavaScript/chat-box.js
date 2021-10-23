8490718b5f3ab59d070e7e921bebe5019841e670
Zansuken <doom.sebastien.pro@gmail.com>
2021-10-23 20:29:52 +0200

    sending message script

---
+++ JavaScript/chat-box.js
@@ -0,0 +1,12 @@
+const sendBtn = document.querySelector("#send_button");
+const textArea = document.querySelector("#text_input");
+const chatFlow = document.querySelector("#chat_flow");
+
+console.dir(textArea)
+
+sendBtn.addEventListener("click", function addMessage() {
+    const chatLine = document.createElement("li")
+    chatLine.textContent = textArea.value
+    chatFlow.appendChild(chatLine)
+    textArea.value = ""
+})
--- JavaScript/side-panel.js
+++ JavaScript/side-panel.js
@@ -2,8 +2,6 @@ const showContactBtn = document.getElementById("nav_btn");
 const hideContactBtn = document.getElementById("nav_btn--close");
 const contactPanel = document.getElementById("contact_panel");
 
-console.log(hideContactBtn);
-
 showContactBtn.addEventListener("click", function openPanel() {
     contactPanel.style.transform = "translateX(0)";
     showContactBtn.style.opacity = "0";
