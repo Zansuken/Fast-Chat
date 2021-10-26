43ec74fd473742b426e828bebdd21174f4f8ba7d
Zansuken <doom.sebastien.pro@gmail.com>
2021-10-26 12:26:57 +0200

    implement login form

--- JavaScript/chat-box.js
+++ JavaScript/chat-box.js
@@ -2,8 +2,6 @@ const sendBtn = document.querySelector("#send_button");
 const textArea = document.querySelector("#text_input");
 const chatFlow = document.querySelector("#chat_flow");
 
-console.dir(textArea)
-
 sendBtn.addEventListener("click", function addMessage() {
     if (textArea.value != "") {
         const chatLine = document.createElement("li")
---
+++ JavaScript/login-panel.js
@@ -0,0 +1,4 @@
+const loginPanel = document.querySelector(".login_section");
+const registerBtn = document.querySelector("#unregistered");
+
+console.dir(loginPanel);
