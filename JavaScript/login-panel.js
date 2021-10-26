a174e302f3cc0f914e2b7428b5127e30764a6d2e
Zansuken <doom.sebastien.pro@gmail.com>
2021-10-26 15:06:42 +0200

    scripting login verification and access to the content

--- JavaScript/login-panel.js
+++ JavaScript/login-panel.js
@@ -1,4 +1,51 @@
-const loginPanel = document.querySelector(".login_section");
-const registerBtn = document.querySelector("#unregistered");
+const loginPanel = document.getElementById("login_section");
+const registerPanel = document.getElementById("register_section");
+const registerLink = document.querySelector("#unregistered");
+const loginLink = document.querySelector("#login_link");
+document.querySelector("#password_field").value;
+let userConnected = false;
 
-console.dir(loginPanel);
+const userIdsTest = [
+    login = "Zansuken",
+    password = "test123"
+]
+
+// Verifying ID's match
+
+const connectBtn = document.querySelector("#connect_button");
+connectBtn.addEventListener("click", function connectingTest() {
+    if (userConnected != true && document.querySelector("#username_field").value === userIdsTest[0] && document.querySelector("#password_field").value === userIdsTest[1]) {
+        console.log("User connected!");
+        loginPanel.style.display = "none";
+        registerPanel.style.display = "none";
+        document.querySelector("#main").style.display = "flex";
+        userConnected = true;
+    } else {
+        console.log("Wrong Username or password!");
+    }
+})
+
+// If user is connected, chat box is displayed
+
+if (userConnected != true) {
+    document.querySelector("#main").style.display = "none";
+}
+
+
+// Register link redirection click (show registering panel)
+
+registerLink.addEventListener("click", function showRegisterForm() {
+    loginPanel.style.display = "none";
+    registerPanel.style.display = "flex";
+})
+
+// Register click event
+
+
+
+// Login link redirection click (show login panel)
+
+loginLink.addEventListener("click", function showRegisterForm() {
+    registerPanel.style.display = "none";
+    loginPanel.style.display = "flex";
+})
