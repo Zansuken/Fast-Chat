5fb56e2cfb1317e171c30c05285e4dc4706fa269
Zansuken <doom.sebastien.pro@gmail.com>
2021-10-26 17:02:20 +0200

    setting default main page state

--- JavaScript/chat-box.js
+++ JavaScript/chat-box.js
@@ -2,6 +2,7 @@ const sendBtn = document.querySelector("#send_button");
 const textArea = document.querySelector("#text_input");
 const chatFlow = document.querySelector("#chat_flow");
 
+
 sendBtn.addEventListener("click", function addMessage() {
     if (textArea.value != "") {
         const chatLine = document.createElement("li")
--- JavaScript/login-panel.js
+++ JavaScript/login-panel.js
@@ -2,10 +2,10 @@ const loginPanel = document.getElementById("login_section");
 const registerPanel = document.getElementById("register_section");
 const registerLink = document.querySelector("#unregistered");
 const loginLink = document.querySelector("#login_link");
-document.querySelector("#password_field").value;
-let userConnected = false;
+const logOutBtn = document.querySelector("#log_out_button");
+let userNotConnected = true;
 
-const userIdsTest = [
+let userIdsTest = [
     login = "Zansuken",
     password = "test123"
 ]
@@ -14,22 +14,53 @@ const userIdsTest = [
 
 const connectBtn = document.querySelector("#connect_button");
 connectBtn.addEventListener("click", function connectingTest() {
-    if (userConnected != true && document.querySelector("#username_field").value === userIdsTest[0] && document.querySelector("#password_field").value === userIdsTest[1]) {
-        console.log("User connected!");
+
+    if (document.querySelector("#username_field").value === userIdsTest[0] && document.querySelector("#password_field").value === userIdsTest[1]) {
         loginPanel.style.display = "none";
         registerPanel.style.display = "none";
         document.querySelector("#main").style.display = "flex";
-        userConnected = true;
+        document.querySelector("#contact_list").style.display = "block";
+        document.querySelector("#password_field").value = "";
+        document.querySelector("#nickname").innerHTML = userIdsTest[0];
+        document.querySelector("#profile_img").style.display = "block";
+        document.querySelector("#nav_btn").style.display = "flex";
+        logOutBtn.style.display = "block";
+        userNotConnected = false;
     } else {
-        console.log("Wrong Username or password!");
+        alert("Wrong Username or password!");
     }
 })
 
-// If user is connected, chat box is displayed
+// Log out event button
 
-if (userConnected != true) {
+logOutBtn.addEventListener("click", function clickToLogOut() {
+    if (userNotConnected === false) {
+        loginPanel.style.display = "block";
+        document.querySelector("#main").style.display = "none";
+        document.querySelector("#contact_list").style.display = "none";
+        document.querySelector("#nickname").innerHTML = "Log in";
+        document.querySelector("#profile_img").style.display = "none";
+        document.querySelector("#nav_btn").style.display = "none";
+        logOutBtn.style.display = "none";
+    }
+})
+
+// If user is not connected, chat box, profile section and contact list is not displayed
+
+if (userNotConnected === true) {
     document.querySelector("#main").style.display = "none";
+    document.querySelector("#contact_list").style.display = "none";
+    logOutBtn.style.display = "none";
+    document.querySelector("#profile_img").style.display = "none";
+    document.querySelector("#nickname").innerHTML = "LOG IN";
+    document.querySelector("#nav_btn").style.display = "none";
 }
+
+// Log in event button
+
+document.querySelector("#nickname").addEventListener("click", function clickToLogIn() {
+    document.querySelector("#username_field").focus();
+})
 
 
 // Register link redirection click (show registering panel)
