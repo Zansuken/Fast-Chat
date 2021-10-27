1cdc6d81a47b300930735a38a9a9802d042419fa
Zansuken <doom.sebastien.pro@gmail.com>
2021-10-27 23:07:15 +0200

    starting Id's method on registering

---
+++ JavaScript/register-panel.js
@@ -0,0 +1,18 @@
+const usernameField = document.querySelector("#register_username_field");
+const passwordField = document.querySelector("#register_password_field");
+const retypePasswordField = document.querySelector("#retype_password");
+const registerBtn = document.querySelector("#register_button");
+
+let username;
+let password;
+let passwordCheck;
+
+
+registerBtn.addEventListener("click", function registerIds() {
+    username = usernameField.value;
+    password = passwordField.value;
+    passwordCheck = retypePasswordField.value;
+    console.log(username);
+    console.log(password);
+    console.log(passwordCheck);
+})
--- JavaScript/side-panel.js
+++ JavaScript/side-panel.js
@@ -1,6 +1,7 @@
 const showContactBtn = document.getElementById("nav_btn");
 const hideContactBtn = document.getElementById("nav_btn--close");
 const contactPanel = document.getElementById("contact_panel");
+const mainBox = document.querySelector("#main"), textBox = document.querySelector("#text_input");
 
 showContactBtn.addEventListener("click", function openPanel() {
     contactPanel.style.transform = "translateX(0)";
@@ -11,3 +12,15 @@ hideContactBtn.addEventListener("click", function closePanel() {
     contactPanel.style.transform = "translateX(-150%)";
     showContactBtn.style.opacity = "1";
 })
+
+mainBox.addEventListener("click", function closePanel() {
+    contactPanel.style.transform = "translateX(-150%)";
+    showContactBtn.style.opacity = "1";
+})
+
+textBox.addEventListener("click", function closePanel() {
+    contactPanel.style.transform = "translateX(-150%)";
+    showContactBtn.style.opacity = "1";
+})
+
+
