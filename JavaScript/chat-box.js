9eafd0cad984bc1130ab063621b07e7de58ece96
Zansuken <doom.sebastien.pro@gmail.com>
2021-10-27 15:08:29 +0200

    changed profile picture from a div to a real img

--- JavaScript/chat-box.js
+++ JavaScript/chat-box.js
@@ -2,6 +2,7 @@ const sendBtn = document.querySelector("#send_button");
 const textArea = document.querySelector("#text_input");
 const chatFlow = document.querySelector("#chat_flow");
 const scrollUpdating = document.querySelector("#main");
+const userImg = document.querySelector("#profile_img");
 
 function updateScroll() {
     scrollUpdating.scrollTop = scrollUpdating.scrollHeight;
@@ -10,12 +11,20 @@ function updateScroll() {
 
 sendBtn.addEventListener("click", function addMessage() {
     if (textArea.value != "") {
-        const chatLine = document.createElement("li")
-        chatLine.textContent = textArea.value
-        chatFlow.appendChild(chatLine)
-        textArea.value = ""
+        const chatLine = document.createElement("li");
+        chatLine.textContent = (document.createElement("div").style.content = userImg.style.content) + textArea.value;
+        chatFlow.appendChild(chatLine);
+        textArea.value = "";
         textArea.focus();
-        updateScroll()
+        updateScroll();
     }
 })
 
+
+const chatImg = document.createElement("img")
+
+chatImg.src = "./Images/Profile\ picture3.jpg"
+
+console.dir(chatImg.src);
+console.dir(userImg);
+// document.createElement("div").style.content = userImg.style.content
