e8bc558e454605bc380695623baae5c9680e2448
Zansuken <doom.sebastien.pro@gmail.com>
2021-10-27 13:15:09 +0200

    scrolling from bottom of chat flow script

--- JavaScript/chat-box.js
+++ JavaScript/chat-box.js
@@ -1,7 +1,11 @@
 const sendBtn = document.querySelector("#send_button");
 const textArea = document.querySelector("#text_input");
 const chatFlow = document.querySelector("#chat_flow");
+const scrollUpdating = document.querySelector("#main");
 
+function updateScroll() {
+    scrollUpdating.scrollTop = scrollUpdating.scrollHeight;
+}
 
 sendBtn.addEventListener("click", function addMessage() {
     if (textArea.value != "") {
@@ -10,5 +14,7 @@ sendBtn.addEventListener("click", function addMessage() {
         chatFlow.appendChild(chatLine)
         textArea.value = ""
         textArea.focus();
+        updateScroll()
     }
 })
+
