9b4b18a1266f736f6c8dbff5c7becb1a913cdfed
Zansuken <doom.sebastien.pro@gmail.com>
2021-10-27 17:26:32 +0200

    designing chat inputs + dates integrations

--- JavaScript/chat-box.js
+++ JavaScript/chat-box.js
@@ -12,17 +12,27 @@ function updateScroll() {
 sendBtn.addEventListener("click", function addMessage() {
     if (textArea.value != "") {
         const chatLine = document.createElement("li");
+        chatLine.textContent = (document.createElement("div").style.content = userImg.style.content) + " : " + textArea.value;
+
         const chatImg = document.createElement("img");
         chatImg.src = userImg.src;
-        chatLine.textContent = (document.createElement("div").style.content = userImg.style.content) + textArea.value;
-        chatLine.append(chatImg);
+
+        let inputTime = document.createElement("span");
+        inputTime.textContent = new Date().toLocaleString({
+            hour: "numeric",
+            minute: "numeric",
+            second: "",
+            weekday: "short",
+            day: "numeric",
+            month: "numeric",
+        });
+
         chatFlow.appendChild(chatLine);
+        chatLine.append(inputTime);
+        chatLine.append(chatImg);
         textArea.value = "";
         textArea.focus();
         updateScroll();
+        console.log(inputTime)
     }
 })
-
-
-
-
